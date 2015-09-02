360e5f4c328c54a696b79b3624b310a0b0890387
Tiffany Kim <kim.tiffany.min@gmail.com>
2015-09-02 18:34:26 +0000

    complete test for unique_id and update quote index page

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+# Quote.first.unique_tag
+
+  test "unique_tag" do
+    quote = Quote.create(:author => 'Abraham Lincoln', :saying => 'Things come to those who wait, but only the things left behind by those who hustle.')
+    expected = 'AL#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
+
   end
+
+
 end
