e5270a2b046a78821683c2ef19657d0e221bb116
Tiffany Kim <kim.tiffany.min@gmail.com>
2015-09-02 20:09:55 +0000

    Add controller test and new show page

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "quote show page" do
+    quote = Quote.create(:author => 'Abraham Lincoln', :saying => 'Things come to those who wait, but only the things left behind by those who hustle.')
+
+    get :show, :id => quote.id
+    assert_response :success
+  end
+
+  test "quote show page, not found" do
+    get :show, :id => 'OMG'
+    assert_response :not_found
+  end
+
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -5,6 +5,7 @@ class QuoteTest < ActiveSupport::TestCase
 
   test "unique_tag" do
     quote = Quote.create(:author => 'Abraham Lincoln', :saying => 'Things come to those who wait, but only the things left behind by those who hustle.')
+    
     expected = 'AL#' + quote.id.to_s
     actual = quote.unique_tag
     assert_equal expected, actual
